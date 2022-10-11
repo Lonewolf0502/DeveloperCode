dd5c71048391bf9681763fd774d2a8c3d0390eaa
ADITYA DUBEY <75172576+adidubs@users.noreply.github.com>
2022-10-11 23:01:34 +0530

    Create Stair1.java
    Climb Stairs with minimum moves using DP.

---
+++ Java/Stair1.java
@@ -0,0 +1,36 @@
+package DynamicProgramming;
+
+import java.util.Scanner;
+
+public class ClimbStairsWithMinimumMoves {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner sc= new Scanner(System.in);
+    int n = sc.nextInt();
+    
+    int[] arr = new int[n];
+    for(int i =0;i<arr.length;i++) {
+    	arr[i] = sc.nextInt();
+    }
+    
+    Integer[] dp = new Integer[n+1];
+    dp[n] = 0;
+    for(int i =n-1;i>= 0;i--) {
+    	if(arr[i]>0) {
+    		int min = Integer.MAX_VALUE;
+    		for(int j =0;j<=arr.length && i+j < dp.length;j++) {
+    			if(dp[i+j]!=null) {
+    				min = Math.min(min, dp[i+j]);
+    			}
+    		}
+    		if(min != Integer.MAX_VALUE) {
+    			dp[i] = min+1;
+    		}
+    	}
+    }
+    System.out.println(dp[0]);
+    
+	}
+
+}
