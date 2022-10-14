60ae2f3e71e60fdbce22c76aa609a8f9b92c0791
bryan <55234458+bryanpratama@users.noreply.github.com>
2022-10-14 11:06:42 +0700

    Create Array in C++.cpp

---
+++ C++/Array in C++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// array declaration by specifying size
+	int arr1[10];
+	
+	// With recent C/C++ versions, we can also
+	// declare an array of user specified size
+	int n = 10;
+	int arr2[n];
+	
+	return 0;
+}
+
+// This code is contributed by bryanpratama
