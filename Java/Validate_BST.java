7fea16d747284d5e466996f28795ade5f1ee5c39
Shrish Dollin <83923398+shrish-tech@users.noreply.github.com>
2022-10-15 10:36:30 +0530

    Created Validate_BST.java
    Checked the validity of the BST, in other words this is the code to check if the given tree input satisfies the requirements or the conditions for it to be a valid Binary Search Tree (Regular & not a self balancing one).

---
+++ Java/Validate_BST.java
@@ -0,0 +1,29 @@
+// 98. Validate Binary Search Tree
+// https://leetcode.com/problems/validate-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) return true;
+        if (root.val >= maxVal || root.val <= minVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+    }
+}
