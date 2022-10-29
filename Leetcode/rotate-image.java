9923c0be52acb661c726f1e6eb58c6dea0f59e06
aggarwalsneha <88716378+aggarwalsneha@users.noreply.github.com>
2022-10-29 12:09:06 +0530

    Create rotate-image.java

---
+++ Leetcode/rotate-image.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int temp;
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=i;j<matrix[i].length;j++)
+            {
+                temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix.length/2;j++)
+            {
+                temp=matrix[i][j];
+                matrix[i][j]=matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j]=temp;
+            }
+        }
+        
+    }
+}
